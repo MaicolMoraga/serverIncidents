9ddd927832228335077dcb1cacd0af42f17f86b8
Maicol Moraga <moraga.maicol@gmail.com>
2020-07-02 01:54:39 -0400

    Initial function
    api is created that calls the complete list of issues
    new agents are created api

---
+++ api.py
@@ -0,0 +1,19 @@
+import requests
+import json
+
+def _url(path):
+    return 'https://5ef62e652c0f2c0016949867.mockapi.io/' + path
+
+def get_mock(path):
+    reply = requests.get(_url(path))
+    print(reply)
+    if reply.status_code != 200:
+        return {'error':1,'menssage':'status code '+reply.status_code}
+    else:
+        return reply.json()
+
+def post_mock(path,json):
+    return requests.post(_url(path),json)
+
+def authorized_agent(user,password):
+    return 0
